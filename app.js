990937bd806da035918213b56747b131bdb8bdf1
Nguyễn Tấn Tân <90363075+NguyenTanTan@users.noreply.github.com>
2023-04-21 08:32:48 +0700

    Add files via upload
    01-Khởi tạo dự án

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var index = require('./routes/index');
+var users = require('./routes/users');
+var tasks = require('./routes/tasks');
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use(express.static('public'));
+
+
+//both index.js and things.js should be in same directory
+app.use('/index', index);
+app.use('/users', users);
+app.use('/tasks', tasks);
+
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+
+app.listen(3000);
